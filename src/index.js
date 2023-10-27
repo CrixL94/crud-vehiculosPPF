08d445bf696ea330b045823c11bbbc6b34969fe9
CrixL94 <lopezvasquezcristhian@gmail.com>
2023-10-27 11:08:34 -0600

    feat  Middleware para configurar CORS

--- src/index.js
+++ src/index.js
@@ -26,6 +26,14 @@ const swaggerSpec = {
 const app = express();
 const port = process.env.PORT || 9000;
 
+// Middleware para configurar CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 //middleware
 app.use(express.json());
 app.use('/api', vehiculosRoutes);
